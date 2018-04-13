ae13d38ac3fefe5cc0d71b580c6c9e29a7303c42
Morten Mjelva <morten.mjelva@gmail.com>
2018-04-13 13:59:47 +0200

    Work

---
+++ Chapters/Chapter2/Listings/smachstate.py
@@ -0,0 +1,12 @@
+class Foo(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['outcome1','outcome2'])
+        self.counter = 0
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state FOO')
+        if self.counter < 3:
+            self.counter += 1
+            return 'outcome1'
+        else:
+            return 'outcome2'
